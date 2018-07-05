33e608fd99d0e6460e2fe0fe8944c742689aa898
Misha <misha1235000@gmail.com>
2018-07-05 14:14:31 +0300

    Initial commit, added all files to the project

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const db = require('./db');
+
+const UserController = require('./user/UserController');
+const AuthController = require('./auth/AuthController');
+
+app.use('/users', UserController);
+app.use('/api/auth', AuthController);
+
+module.exports = app;
